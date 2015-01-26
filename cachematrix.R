e8b75cab50e43904d0feee8b8a2c593e9d130c7b
jvelaga <jvelaga1@yahoo.com>
2015-01-25 19:42:59 -0500

    Jag assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## intakes a matrix and keeps in cache
+makeCacheMatrix <- function(x = matrix()) 
+{
+    cacheI <- NULL
+    
+    set <- function(y)
+    {
+        x <<- y
+        cacheI <<- NULL
+    }
+    get <- function() x
+    
+    setCache <- function(y) cacheI <<- y
+    getCache <- function() cacheI
+    
+    list(set = set, get = get, setCache = setCache, getCache = getCache)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## For a matrix that is not in cache creates and returns inverse matrix.
+cacheSolve <- function(x, ...) 
+{
+    z <- makeCacheMatrix(x)
         ## Return a matrix that is the inverse of 'x'
+    if(!is.null(z$getCache())) 
+            return(z$getCache())
+       else
+           z$setCache(solve(z$get()))
 }
+
